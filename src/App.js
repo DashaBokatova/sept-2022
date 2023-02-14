e5886584ce4b0962a1540abc4cc0096002cdcfb5
DashaBokatova <dashabokatova2001@gmail.com>
2023-02-14 19:30:36 +0200

    homework2/react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-const App = () => {
-  return (
-      <div>
-        Lesson2
-      </div>
-  );
-};
+import React, { useEffect, useState } from "react";
+import './App.css';
+import { Users } from "./components/Users";
+import { UserDetails } from "./components/UserDetails";
+import { getUsers } from "./api/getUsers";
+
+function App() {
+    const [users, setUsers] = useState([]);
+    const [userId, setUserId] = useState(null);
+
+    useEffect(() => {
+        getUsers().then(resp => setUsers(resp));
+    }, []);
+
+    return (
+        <div className="App">
+            <div className="info-block">
+                {!!users.length && <Users users={users} onSelectUser={setUserId}/>}
+            </div>
+            <div className="divider"/>
+            <div className="info-block">
+                {!!userId && <UserDetails onRemoveUserDetails={setUserId} userId={userId}/>}
+            </div>
+        </div>
+    );
+}
 
 export {App};
